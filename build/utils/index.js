6c3cbb0544ebf1c963431b092f093aba5786cdc9
Sofikul mallick <sofikul.projects@gmail.com>
2022-09-10 21:40:54 +0530

    Clean up unnecessary codes

--- build/utils/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const auth_1 = require("./auth");
-exports.default = {
-    getCurrentUser: auth_1.getCurrentUser,
-    isAuthenticated: auth_1.isAuthenticated,
-    isAuthorized: auth_1.isAuthorized,
-};
